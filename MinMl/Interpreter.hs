cb0594f1222371886b77c35557c48c69d200622c
JesusHaans <jesuslopez@ciencias.unam.mx>
2022-10-27 18:06:15 -0500

    fixing interpreter

--- MinMl/Interpreter.hs
+++ MinMl/Interpreter.hs
@@ -1,5 +1,5 @@
 module MinMl.Interpreter where
-
+import Data.List
 import MinMl.Parser
 import MinMl.Lexer
 
@@ -15,17 +15,12 @@ evaln e = if e == e' then e else evaln e'
     where e' = evalg e
 
 evalg:: MinMl -> MinMl
-evalg (Num i) = Num i
-evalg (B b) = B b
-evalg (Var x) = error "Variable no definida"
 evalg (If e1 e2 e3) = case evalg e1 of
     (B True) -> evalg e2
     (B False) -> evalg e3
     e1' -> If e1' e2 e3
-evalg (Let x e1 e2) = evalg (subst (evalg e2) x (evalg e1))
-evalg (LetRec f e1 e2) = error "LetRec no implementado"
 evalg (App e1 e2) = case evalg e1 of
-    (Fun x e1') -> evalg (subst e1' x (evalg e2))
+    (Fun x e1') -> evalg (subst e1' (x, evalg e2))
     e1' -> App e1' e2 
 evalg (Sum e1 e2) = case (evalg e1, evalg e2) of
     (Num i1, Num i2) -> Num (i1 + i2)
@@ -58,25 +53,63 @@ evalg (Or e1 e2) = case (evalg e1, evalg e2) of
     (B b1, B b2) -> B (b1 || b2)
     (e1', e2') -> Or e1' e2'
 evalg (Fun x e) = Fun x (evalg e)
+evalg (Let x e1 e2) = evalg (subst e2 (x, evalg e1))
+evalg (LetRec f e1 e2) = evalg (subst e2 (f, Fix f e1))
+evalg s@(Fix f e) = evalg (subst e (f,s))   -- -> asi tenemos que Fix f -> f (Fix f) (como en ejemplo de lab)
 
 
 
-subst:: MinMl -> String -> MinMl -> MinMl
-subst (Num i) x e = Num i
-subst (B b) x e = B b
-subst (Var y) x e = if x == y then e else Var y
-subst (If e1 e2 e3) x e = If (subst e1 x e) (subst e2 x e) (subst e3 x e)
-subst (Let y e1 e2) x e = Let y (subst e1 x e) (subst e2 x e)
-subst (LetRec f e1 e2) x e = LetRec f (subst e1 x e) (subst e2 x e)
-subst (App e1 e2) x e = App (subst e1 x e) (subst e2 x e)
-subst (Sum e1 e2) x e = Sum (subst e1 x e) (subst e2 x e)
-subst (Res e1 e2) x e = Res (subst e1 x e) (subst e2 x e)
-subst (Mul e1 e2) x e = Mul (subst e1 x e) (subst e2 x e)
-subst (Lt e1 e2) x e = Lt (subst e1 x e) (subst e2 x e)
-subst (Gt e1 e2) x e = Gt (subst e1 x e) (subst e2 x e)
-subst (Le e1 e2) x e = Le (subst e1 x e) (subst e2 x e)
-subst (Ge e1 e2) x e = Ge (subst e1 x e) (subst e2 x e)
-subst (Eq e1 e2) x e = Eq (subst e1 x e) (subst e2 x e)
-subst (And e1 e2) x e = And (subst e1 x e) (subst e2 x e)
-subst (Or e1 e2) x e = Or (subst e1 x e) (subst e2 x e)
-subst (Fun y e1) x e = Fun y (subst e1 x e)
+-- substitucion con chochos ya que en p2 no cheque las variables libres
+
+subst:: MinMl -> (String, MinMl) -> MinMl
+subst (Num i) _ = Num i
+subst (B b) _ = B b
+subst (Var x) (y, e) = if x == y then e else Var x
+subst (If e1 e2 e3) s = If (subst e1 s) (subst e2 s) (subst e3 s)
+subst e@(Let id e1 e2) s@(x, e')
+    | id `notElem` (fv e') && (x /= id ) = Let x (subst e1 s) (subst e2 s)
+    | otherwise = e
+subst e@(LetRec id e1 e2) s@(x, e')
+    | id `notElem` (fv e') && (x /= id ) = LetRec x (subst e1 s) (subst e2 s)
+    | otherwise = e
+subst (App e1 e2) s = App (subst e1 s) (subst e2 s)
+subst (Sum e1 e2) s = Sum (subst e1 s) (subst e2 s)
+subst (Res e1 e2) s = Res (subst e1 s) (subst e2 s)
+subst (Mul e1 e2) s = Mul (subst e1 s) (subst e2 s)
+subst (Lt e1 e2) s = Lt (subst e1 s) (subst e2 s)
+subst (Gt e1 e2) s = Gt (subst e1 s) (subst e2 s)
+subst (Le e1 e2) s = Le (subst e1 s) (subst e2 s)
+subst (Ge e1 e2) s = Ge (subst e1 s) (subst e2 s)
+subst (Eq e1 e2) s = Eq (subst e1 s) (subst e2 s)
+subst (And e1 e2) s = And (subst e1 s) (subst e2 s)
+subst (Or e1 e2) s = Or (subst e1 s) (subst e2 s)
+subst (Fun x e) s@(y, e')
+    | x `notElem` (fv e') && (x /= y) = Fun x (subst e s)
+    | otherwise = Fun x e
+subst (Fix id e) s = Fix id (subst e s)
+
+
+
+
+-- para checar variables libres
+
+fv:: MinMl -> [String]
+fv (Num i) = []
+fv (B b) = []
+fv (Var x) = [x]
+fv (If e1 e2 e3) = (fv e1) ++ (fv e2) ++ (fv e3)
+fv (Let x e1 e2) = ((fv e1)\\[x]) ++ ((fv e2)\\[x])
+fv (LetRec x e1 e2) = ((fv e1)\\[x]) ++ ((fv e2)\\[x])
+fv (App e1 e2) = (fv e1) ++ (fv e2)
+fv (Sum e1 e2) = (fv e1) ++ (fv e2)
+fv (Res e1 e2) = (fv e1) ++ (fv e2)
+fv (Mul e1 e2) = (fv e1) ++ (fv e2)
+fv (Lt e1 e2) = (fv e1) ++ (fv e2)
+fv (Gt e1 e2) = (fv e1) ++ (fv e2)
+fv (Le e1 e2) = (fv e1) ++ (fv e2)
+fv (Ge e1 e2) = (fv e1) ++ (fv e2)
+fv (Eq e1 e2) = (fv e1) ++ (fv e2)
+fv (And e1 e2) = (fv e1) ++ (fv e2)
+fv (Or e1 e2) = (fv e1) ++ (fv e2)
+fv (Fun x e) = (fv e)\\[x]
+fv (Fix x e) = (fv e)\\[x]
