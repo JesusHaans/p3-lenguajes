e901e6497664364c92d7e08eb7218fe04612f6ad
JesusHaans <jesuslopez@ciencias.unam.mx>
2022-10-28 13:29:42 -0500

    fixing the FV what give me problem to evalg but still have problem with test 5 of MiniMl

--- MinMl/Interpreter.hs
+++ MinMl/Interpreter.hs
@@ -8,8 +8,8 @@ eval:: MinMl -> Either Int Bool
 eval e = case evaln e of
     (Num i) -> Left i
     (B b) -> Right b
-    e' -> error "Exprecion Bloqueada"
-
+    e' -> error ( show e')
+ 
 evaln:: MinMl -> MinMl
 evaln e = if e == e' then e else evaln e'
     where e' = evalg e
@@ -18,13 +18,6 @@ evalg:: MinMl -> MinMl
 evalg (Num x) = Num x
 evalg (B x) = B x
 evalg (Var x)  = Var x
-evalg (If e1 e2 e3) = case evalg e1 of
-    (B True) -> evalg e2
-    (B False) -> evalg e3
-    e1' -> If e1' (evalg e2) (evalg e3)
-evalg (App e1 e2) = case evalg e1 of
-    (Fun x e1') -> evalg (subst e1' (x, evalg e2))
-    e1' -> App e1' (evalg e2) 
 evalg (Sum e1 e2) = case (evalg e1, evalg e2) of
     (Num i1, Num i2) -> Num (i1 + i2)
     (e1', e2') -> Sum e1' e2'
@@ -55,11 +48,17 @@ evalg (And e1 e2) = case (evalg e1, evalg e2) of
 evalg (Or e1 e2) = case (evalg e1, evalg e2) of
     (B b1, B b2) -> B (b1 || b2)
     (e1', e2') -> Or e1' e2'
+evalg (If e1 e2 e3) = case evalg e1 of
+    (B True) -> evalg e2
+    (B False) -> evalg e3
+    e1' -> If e1' e2 e3
+evalg (App e1 e2) = case evalg e1 of
+    (Fun x e1') -> evalg (subst e1' (x, e2))
+    e1' -> App e1' (evalg e2) 
 evalg (Fun x e) = Fun x (evalg e)
-evalg (Let x e1 e2) = evalg (subst e2 (x, evalg e1))
+evalg (Let x e1 e2) = evalg (subst e2 (x, e1))
 evalg (LetRec f e1 e2) = evalg (subst e2 (f, Fix f e1))
-evalg s@(Fix f e) = evalg (subst e (f,s))   -- -> asi tenemos que Fix f -> f (Fix f) (como en ejemplo de lab)
-
+evalg e@(Fix id f) = evalg $ subst f (id,e)  -- -> asi tenemos que Fix f -> f (Fix f) (como en ejemplo de lab)
 
 
 -- substitucion con chochos ya que en p2 no cheque las variables libres
@@ -69,7 +68,7 @@ subst (Num i) _ = Num i
 subst (B b) _ = B b
 subst (Var x) (y, e) = if x == y then e else Var x
 subst (If e1 e2 e3) s = If (subst e1 s) (subst e2 s) (subst e3 s)
-subst e@(Let id e1 e2) s@(x, e')
+subst e@(Let id e1 e2) s@(x, e') 
     | id `notElem` (fv e') && (x /= id ) = Let x (subst e1 s) (subst e2 s)
     | otherwise = e
 subst e@(LetRec id e1 e2) s@(x, e')
@@ -101,8 +100,8 @@ fv (Num i) = []
 fv (B b) = []
 fv (Var x) = [x]
 fv (If e1 e2 e3) = (fv e1) ++ (fv e2) ++ (fv e3)
-fv (Let x e1 e2) = ((fv e1)\\[x]) ++ ((fv e2)\\[x])
-fv (LetRec x e1 e2) = ((fv e1)\\[x]) ++ ((fv e2)\\[x])
+fv (Let x e1 e2) = filter (/=x) (fv e1) ++ filter (/=x) (fv e2)
+fv (LetRec x e1 e2) = filter (/=x) (fv e1) ++ filter (/=x) (fv e2)
 fv (App e1 e2) = (fv e1) ++ (fv e2)
 fv (Sum e1 e2) = (fv e1) ++ (fv e2)
 fv (Res e1 e2) = (fv e1) ++ (fv e2)
@@ -114,5 +113,12 @@ fv (Ge e1 e2) = (fv e1) ++ (fv e2)
 fv (Eq e1 e2) = (fv e1) ++ (fv e2)
 fv (And e1 e2) = (fv e1) ++ (fv e2)
 fv (Or e1 e2) = (fv e1) ++ (fv e2)
-fv (Fun x e) = (fv e)\\[x]
-fv (Fix x e) = (fv e)\\[x]
+fv (Fun x e) = filter (/=x) (fv e)
+fv (Fix x e) = filter (/=x) (fv e)
+
+
+
+
+
+
+---- IntMl.eval (ParMl.parser $ LexMl.lexer "let y = 2 in ( y * 10) end") ----
